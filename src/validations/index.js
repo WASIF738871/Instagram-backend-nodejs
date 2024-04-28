046ffbd5ffc0b05aba397342cae665323c694a19
MOHAMMAD WASIF <erwasifmohd786@gmail.com>
2024-04-28 10:10:14 +0530

    Deleted review.validation and tour.validation

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,6 +1,4 @@
 import customValidations from './custom.validations.js';
-import tourValidations from './tour.validations.js';
 import userValidations from './user.validations.js';
-import reviewValidations from './review.validations.js';
 
-export { customValidations, tourValidations, userValidations, reviewValidations };
+export { customValidations, userValidations };
